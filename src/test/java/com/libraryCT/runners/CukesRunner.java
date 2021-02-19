6440c8ba0acade15c0369e5b46d7d5190fbd1b10
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-02-19 01:36:11 -0600

    Initial framework commit.

---
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.libraryCT.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "com/libraryCT/step_definitions",
+        dryRun = false,
+        tags = "@student or @librarian"
+)
+public class CukesRunner {
+
+
+}
